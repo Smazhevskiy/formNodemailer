fa78a8deca28106c80533ca19681b022445fb9a5
user_nitro_5 <timmy.lepel@gmail.com>
2021-12-15 20:44:17 +0300

    Initial commit

---
+++ index.js
@@ -0,0 +1,52 @@
+const express = require('express')
+const bodyParser = require('body-parser')
+const nodemailer = require('nodemailer')
+const app = express()
+require('dotenv').config()
+// Parse incoming requests data (https://github.com/expressjs/body-parser)
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({extended: false}))
+const route = express.Router()
+
+const port = 3010
+
+app.use('/v1', route)
+
+app.listen(port, () => {
+    console.log(`Server listening on port ${port}`)
+})
+
+
+const transporter = nodemailer.createTransport({
+    service: 'gmail',
+    tls: {
+        rejectUnauthorized: false
+    },
+    auth: {
+        user: process.env.GMAIL_USER,
+        pass: process.env.GMAIL_PASS,
+    },
+})
+
+
+app.post('/sendMessage', async (req, res) => {
+    const {name, contacts, message} = req.body
+    const mailData = {
+        from: name,
+        to: 'a.zmashevskiy@gmail.com',
+        subject: contacts,
+        text: message,
+        html: `<b>Сообщение c формы</b>
+<div>name: ${name}</div>
+<div>contacts: ${contacts}</div>
+<div>message: ${message}</div>`,
+    }
+
+    await transporter.sendMail(mailData, (error, info) => {
+        if(error) {
+            res.status(400).send({error: 'Ошибка'})
+            return console.log(error)
+        }
+        res.status(200).send({message: 'Mail send'})
+    })
+})
