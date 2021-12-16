2ebd789a5d41e37750a8ed595c0671b5f90b3641
user_nitro_5 <timmy.lepel@gmail.com>
2021-12-16 19:44:27 +0300

    env

--- index.js
+++ index.js
@@ -7,12 +7,28 @@ require('dotenv').config()
 
 app.use(express.json())
 app.use(cors()) //{origin: 'https://github.com/Smazhevskiy'}
-app.options('*', cors());
+app.options('*', cors())
 
 
 app.get('/', cors(), (req, res) => {
-    res.send('hello world работает')
+    res.send('server is running')
 })
+
+
+const mailData = (name, contacts, message) => {
+    return (
+        {
+            from: 'Письмо с формы обратной связи',
+            subject: 'HR WANTS ME',
+            to: process.env.MAIL_TO || 'a.zmashevskiy@gmail.com',
+            html: `<b>Yoo</b>
+<div>name: ${name}</div>
+<div>contacts: ${contacts}</div>
+<div>message: ${message}</div>`,
+        }
+    )
+}
+
 
 
 const transporter = nodemailer.createTransport({
@@ -21,32 +37,20 @@ const transporter = nodemailer.createTransport({
         rejectUnauthorized: false
     },
     auth: {
-        user: process.env.GMAIL_USER,
+        user: process.env.GMAIL_LOGIN,
         pass: process.env.GMAIL_PASS,
     },
 })
 
 
-app.post('/sendMessage', cors(),  async (req, res) => {
+app.post('/sendMessage', cors(), async (req, res) => {
     const {name, contacts, message} = req.body
-
-    const mailData = {
-        from: name,
-        to: 'a.zmashevskiy@gmail.com',
-        subject: contacts,
-        text: message,
-        html: `<b>Сообщение c формы</b>
-<div>name: ${name}</div>
-<div>contacts: ${contacts}</div>
-<div>message: ${message}</div>`,
-    }
-
-   await transporter.sendMail(mailData, (error, info) => {
+    await transporter.sendMail(mailData(name, contacts, message), (error, info) => {
         if(error) {
             res.status(400).send({error: error})
             return console.log(error)
         }
-        res.status(200).send({message: 'Mail send'})
+        res.status(200).send({message: 'Mail send' + info})
     })
 })
 
