ac2a67bf3396f70a1505bd781ae8b8b9e1e9ac6d
user_nitro_5 <timmy.lepel@gmail.com>
2022-01-14 20:36:25 +0300

    html css body

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
+
 const express = require('express')
 const nodemailer = require('nodemailer')
 const cors = require('cors')
-const {HtmlMail} = require('./htmlMail')
+
+
+
+
 const port = process.env.PORT || 3010
 const app = express()
 require('dotenv').config()
@@ -16,13 +20,64 @@ app.get('/', cors(), (req, res) => {
 })
 
 
+
+
 const mailData = (name, contacts, message) => {
     return (
         {
             from: 'Письмо с формы обратной связи',
             subject: 'HR WANTS ME',
             to: process.env.MAIL_TO || 'a.zmashevskiy@gmail.com',
-            html: HtmlMail,
+            html: `<div
+            style="
+        box-sizing: border-box;
+        width: 400px;
+        font-size: 20px;
+        background-color: #000;
+      "
+        >
+            <div>
+                <img
+                    style="background-size: cover; width: 100%;"
+                    src="https://f.vividscreen.info/soft/c79bb3903c454eb6111ca201e03694e9/Mountain-Lion-Nyan-Cat-320x240.jpg"
+                    alt="nyanCat"
+                />
+            </div>
+            <div
+                style="
+          color: cornsilk;
+          padding-bottom: 8px;
+          box-sizing: content-box;
+          overflow: hidden;
+          word-wrap: break-word;
+        "
+            >
+                <div
+                    style="
+            padding: 5px;
+            box-shadow: 0 4px 4px #b4b7a7, 0 1px 2px #d2cbcb;
+          "
+                >
+                    name: ${name}
+                </div>
+                <div
+                    style="
+            padding: 5px;
+            box-shadow: 0 4px 4px #b4b7a7, 0 1px 2px #d2cbcb;
+          "
+                >
+                    contacts: ${contacts}
+                </div>
+                <div
+                    style="
+            padding: 5px;
+            box-shadow: 0 4px 4px #b4b7a7, 0 1px 2px #d2cbcb;
+          "
+                >
+                    message:${message}
+                </div>
+            </div>
+        </div>`,
         }
     )
 }
